8aa999ef670dba3d0c9f0ce507d5125a88ac00cd
rxvinicius <vinicius-rodrigues2000@hotmail.com>
2024-02-15 11:05:00 -0300

    feat: add services section

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button";
 export { default as Nav } from "./Nav";
-export { default as ShoeCard } from "./ShoeCard";
 export { default as PopularProductCard } from "./PopularProductCard";
+export { default as ServiceCard } from "./ServiceCard";
+export { default as ShoeCard } from "./ShoeCard";
